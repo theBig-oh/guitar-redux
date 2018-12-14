55990ff679cfd9ebaae53f46c0b18d3dfe0d147e
Oscar <Omedrano564@gmail.com>
2018-12-14 14:29:04 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,24 +30,7 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-/*      {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            loader: "style-loader!css-loader!sass-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
-      }*/
+
             {
         test: /\.(scss|css)$/,
         loader:"style-loader!css-loader!sass-loader"
